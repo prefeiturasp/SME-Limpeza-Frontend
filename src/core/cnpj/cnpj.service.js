fca47e1dfb2a5b72a3ea1f9c75d98267977c9fd1
Jardeon J M Araujo <70022376+JardeonJMAraujo@users.noreply.github.com>
2026-07-15 12:12:24 -0300

    Refatoração conforme o sonar

--- src/core/cnpj/cnpj.service.js
+++ src/core/cnpj/cnpj.service.js
@@ -29,16 +29,6 @@
         const pesosDV1 = [5,4,3,2,9,8,7,6,5,4,3,2];
         const pesosDV2 = [6,5,4,3,2,9,8,7,6,5,4,3,2];
 
-        function valorCaracter(c) {
-            if (c >= '0' && c <= '9')
-                return c.codePointAt(0) - 48;
-
-            if (c >= 'A' && c <= 'Z')
-                return c.codePointAt(0) - 48;
-
-            return -1;
-        }
-
         function calcularDV(base, pesos) {
             let soma = 0;
 
@@ -62,6 +52,16 @@
         if (dv2 === null) return false;
 
         return dvInformado === `${dv1}${dv2}`;
+    }
+
+    function valorCaracter(c) {
+        if (c >= '0' && c <= '9')
+            return c.codePointAt(0) - 48;
+
+        if (c >= 'A' && c <= 'Z')
+            return c.codePointAt(0) - 48;
+
+        return -1;
     }
 
     angular
